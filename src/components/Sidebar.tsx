0dc4e2ed42920d17eed742484caac070ec76e678
Moayed <hysoca7@gmail.com>
2024-06-18 14:35:04 +0400

    remove theme toggler, change green to blue, adjust sidebar image

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,70 +1,77 @@
-'use client';
+"use client";
 
-import {AiFillGithub,AiFillLinkedin,AiFillTwitterCircle,AiFillYoutube} from 'react-icons/ai'
-import { GoLocation } from 'react-icons/go'
-import { GiTie } from 'react-icons/gi'
-import { useTheme } from 'next-themes';
-import { ThemeSwitcher } from './ThemeSwitcher';
+import {
+  AiFillGithub,
+  AiFillLinkedin,
+  AiFillTwitterCircle,
+  AiFillYoutube,
+} from "react-icons/ai";
+import { GoLocation } from "react-icons/go";
+import { GiTie } from "react-icons/gi";
+import { useTheme } from "next-themes";
+import { ThemeSwitcher } from "./ThemeSwitcher";
 import Image from "next/legacy/image";
-const Sidebar = () =>{
+const Sidebar = () => {
+  const { theme, setTheme } = useTheme();
+  const changeTheme = () => {
+    setTheme(theme === "light" ? "dark" : "light");
+  };
+  return (
+    <div>
+      <img
+        src="/imgs/Moayed.jpg"
+        width="120"
+        height="120"
+        alt="here is an image"
+        className=" mx-auto rounded-full min-w-none min-h-none w-[5rem] h-[8rem]"
+      />
 
-    const {theme,setTheme} = useTheme()
-    const changeTheme = () =>{
-        setTheme(
-            theme === 'light' ? 'dark' : 'light'
-        )
-    }
-    return (
-        <div>
-         
-                 <Image src="/imgs/Moayed.jpg"  width="120" height="120"  quality="100"
+      <h3 className="my-4 text-3xl font-medium tracking-wider font-header">
+        <span className="text-green">Moayed </span>
+        Eisa
+      </h3>
+      <p className="px-2 py-1 my-3 bg-gray-200 dark:bg-dark-200 rounded-full">
+        Web Developer
+      </p>
+      <a
+        className="flex items-center justify-center px-2 py-1 my-3 bg-gray-200 dark:bg-dark-200 rounded-full "
+        href="pdf/Moayed.pdf"
+        download="Moayed"
+      >
+        <GiTie className="w-6 h-6" />
+        <span>Download Resume</span>
+      </a>
+      <div className="flex justify-around my-5 text-green w-9/12 md:w-full mx-auto">
+        <a href="https://twitter.com/Moayed_2020">
+          <AiFillTwitterCircle className="w-8 h-8 cursor-pointer" />
+        </a>
 
-         alt="here is an image"
-         className=' mx-auto rounded-full'
-         />
-        
-         <h3 className='my-4 text-3xl font-medium tracking-wider font-header'>
-            <span className='text-green'>Moayed </span>
-            Eisa
-        </h3>
-        <p className='px-2 py-1 my-3 bg-gray-200 dark:bg-dark-200 rounded-full'>Web Developer</p>
-        <a className='flex items-center justify-center px-2 py-1 my-3 bg-gray-200 dark:bg-dark-200 rounded-full ' 
-        href='pdf/Moayed.pdf'
-         download='Moayed'>
-            <GiTie className='w-6 h-6'/> 
-            <span>Download Resume</span>
-            </a>
-        <div className='flex justify-around my-5 text-green w-9/12 md:w-full mx-auto'>
-            <a href="https://twitter.com/Moayed_2020">
-                <AiFillTwitterCircle className='w-8 h-8 cursor-pointer' />
-            </a>
-            
-            <a href="https://github.com/MrMaximeliom">
-                <AiFillGithub className='w-8 h-8 cursor-pointer' />
-            </a>
-            <a href="https://www.linkedin.com/in/moayedeisa/">
-                <AiFillLinkedin className='w-8 h-8 cursor-pointer' />
-            </a>
+        <a href="https://github.com/MrMaximeliom">
+          <AiFillGithub className="w-8 h-8 cursor-pointer" />
+        </a>
+        <a href="https://www.linkedin.com/in/moayedeisa/">
+          <AiFillLinkedin className="w-8 h-8 cursor-pointer" />
+        </a>
+      </div>
+      <div
+        className="my-5 py-4 bg-gray-200 dark:bg-dark-200"
+        style={{ marginLeft: "-1rem", marginRight: "-1rem" }}
+      >
+        <div className="flex items-center justify-center space-x-2 ">
+          <GoLocation />
+          <span>Dubai , UAE</span>
         </div>
-        <div className='my-5 py-4 bg-gray-200 dark:bg-dark-200' style={{ marginLeft:'-1rem',marginRight:'-1rem' }}>
-            <div className='flex items-center justify-center space-x-2 '>
-                <GoLocation/>
-                <span>Dubai , UAE</span>
-            </div>
-            <p className='my-2'>moayed.abdulhafiez@gmail.com</p>
-            <p className='my-2'>+971569233853</p>
-        </div>
-        {/* email button */}
-        <button className='bg-gradient-to-r from-green to-blue-400 w-8/12 rounded-full my-2 py-2 focus:outline-none'
-        onClick={() => window.open('mailto:moayed.abdulhafiez@gmail.com')}
-        >
-            Email Me
-            </button>
-
-  <ThemeSwitcher/>
-
-        </div>
-      
-    )
-}
-export default Sidebar
+        <p className="my-2">moayed.abdulhafiez@gmail.com</p>
+        <p className="my-2">+971569233853</p>
+      </div>
+      {/* email button */}
+      <button
+        className="bg-gradient-to-r from-green to-blue-400 w-8/12 rounded-full my-2 py-2 focus:outline-none"
+        onClick={() => window.open("mailto:moayed.abdulhafiez@gmail.com")}
+      >
+        Email Me
+      </button>
+    </div>
+  );
+};
+export default Sidebar;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,7 @@
 import type { Config } from "tailwindcss";
 const config: Config = {
-  darkMode:'class',
-  
+  darkMode: "class",
+
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -9,19 +9,13 @@ const config: Config = {
   ],
   theme: {
     fontFamily: {
-      "header": "var(--kaushan-reqular)",
-      "second": "var(--single-day-reqular)",
+      header: "var(--kaushan-reqular)",
+      second: "var(--single-day-reqular)",
     },
-    boxShadow:{
-      "custom-light":" 0 0 10px #313131",
-      "custom-dark": "5px 5px 10px #0a0c0e , -5px -5px 10px #14161c"
+    boxShadow: {
+      "custom-light": " 0 0 10px #313131",
+      "custom-dark": "5px 5px 10px #0a0c0e , -5px -5px 10px #14161c",
     },
-
-
-
-
-
-   
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
@@ -30,25 +24,23 @@ const config: Config = {
       },
       colors: {
         green: {
-           DEFAULT: '#00f260',
+          DEFAULT: "#436CC9",
         },
-        dark:{
-          DEFAULT:"#010101",
-          100:"#0a0b0e",
-          200:"#16181d",
-          300:"#16181d",
-          500:"#0f1115",
-          700:"#202125"
-        }
-     },
-     
-      
+        dark: {
+          DEFAULT: "#010101",
+          100: "#0a0b0e",
+          200: "#16181d",
+          300: "#16181d",
+          500: "#0f1115",
+          700: "#202125",
+        },
+      },
     },
   },
-  variants:{
-    extend:{
-      boxShadow:["dark"]
-    }
+  variants: {
+    extend: {
+      boxShadow: ["dark"],
+    },
   },
   plugins: [],
 };
